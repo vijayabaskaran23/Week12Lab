276407c5aca10e63dd1f55a4d81de0cfbe735362
vijayabaskaran23 <57746329+vijayabaskaran23@users.noreply.github.com>
2019-11-21 01:02:06 -0700

    created first function

---
+++ Week12-utility.py
@@ -0,0 +1,7 @@
+# https://github.com/vijayabaskaran23/Week12Lab.git
+#Ethan Vijayabaskaran
+#CSCI 102 - Section E
+#Lab 12 - Part A
+def PrintOutput(output):
+    out = 'OUTPUT ' + str(output)
+    print(out)
